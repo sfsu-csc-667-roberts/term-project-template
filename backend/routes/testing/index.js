9eb73d1e765b1ceb652d059225576d46f0bb6c6a
John Roberts <roberts.john@gmail.com>
2023-04-06 19:34:16 -0700

    Added test routes

---
+++ backend/routes/testing/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../../db/connection.js");
+
+router.get("/", async (_request, response) => {
+  await db
+    .any('INSERT INTO test_table ("test_string") VALUES ($1)', [
+      `Hello on ${new Date().toLocaleDateString("en-us", {
+        hour: "numeric",
+        minute: "numeric",
+        month: "short",
+        day: "numeric",
+        weekday: "long",
+        year: "numeric",
+      })}`,
+    ])
+    .catch((error) => {
+      console.log({ error });
+      response.json({ error });
+    });
+
+  const rows = await db.any("SELECT * FROM test_table").catch((error) => {
+    console.log({ error });
+    response.json({ error });
+  });
+
+  response.json(rows);
+
+  // db.any(`INSERT INTO test_table ("test_string") VALUES ($1)`, [
+  //   `Hello on ${new Date().toLocaleDateString("en-us", {
+  //     hour: "numeric",
+  //     minute: "numeric",
+  //     month: "short",
+  //     day: "numeric",
+  //     weekday: "long",
+  //     year: "numeric",
+  //   })}`,
+  // ])
+  //   .then((_) => db.any(`SELECT * FROM test_table`))
+  //   .then((results) => response.json(results))
+  //   .catch((error) => {
+  //     console.log(error);
+  //     response.json({ error });
+  //   });
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -24,11 +24,14 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "static")));
 
 const routes = require("./routes/static.js");
+const testRoutes = require("./routes/testing/index.js");
+
 app.use("/", routes.homeRoutes);
 app.use("/games", routes.gameRoutes);
 app.use("/lobby", routes.lobbyRoutes);
 app.use("/profile", routes.profileRoutes);
 app.use("/sign-up", routes.signUpRoutes);
+app.use("/test", testRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
