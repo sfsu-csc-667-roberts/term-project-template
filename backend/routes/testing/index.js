3e5a83d6091c4e6aa3b5cace46da9842137101c4
John Roberts <roberts.john@gmail.com>
2023-04-06 19:42:56 -0700

    Added postinstall command

--- backend/routes/testing/index.js
+++ backend/routes/testing/index.js
@@ -25,23 +25,6 @@ router.get("/", async (_request, response) => {
   });
 
   response.json(rows);
-
-  // db.any(`INSERT INTO test_table ("test_string") VALUES ($1)`, [
-  //   `Hello on ${new Date().toLocaleDateString("en-us", {
-  //     hour: "numeric",
-  //     minute: "numeric",
-  //     month: "short",
-  //     day: "numeric",
-  //     weekday: "long",
-  //     year: "numeric",
-  //   })}`,
-  // ])
-  //   .then((_) => db.any(`SELECT * FROM test_table`))
-  //   .then((results) => response.json(results))
-  //   .catch((error) => {
-  //     console.log(error);
-  //     response.json({ error });
-  //   });
 });
 
 module.exports = router;
